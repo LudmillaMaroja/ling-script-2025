e3838ea59ee1d04b951280b4cc91a3436ceef1ff
Ludmilla Maroja <90078095+LudmillaMaroja@users.noreply.github.com>
2025-05-05 10:59:45 -0300

    exercício-linguagemdescript

---
+++ operacoes-ls-2025/objeto_exames/script.js
@@ -0,0 +1,16 @@
+class Exam{
+    constructor(answer, weight){
+        this.answer = answer;
+        this.weight = weight;
+        this.result = [];
+    }
+
+    add(prova){
+
+    }
+    avg(){}
+    min(){}
+    max(){}
+    lt(){}
+    gt(){}
+}
