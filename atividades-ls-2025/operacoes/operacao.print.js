2ccf18d64a81f729154a9ffe5c540ece1a3f71a9
Ludmilla Maroja <90078095+LudmillaMaroja@users.noreply.github.com>
2025-05-23 20:26:33 -0300

    Update operacao.print.js

--- atividades-ls-2025/operacoes/operacao.print.js
+++ atividades-ls-2025/operacoes/operacao.print.js
@@ -1,8 +1,7 @@
-import { sum, soma, product, sumOdds } from "./operacao.js";
+import { sum, product, sumOdds } from "./operacao.js";
 
 let vetor = [1,2,3,4,5,6];
 
 console.log(sum(vetor));
 console.log(product(vetor));
 console.log(sumOdds(vetor));
-console.log(soma(vetor));
