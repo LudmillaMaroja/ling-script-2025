f2700ce01f2b97e3e64a63d26f1c524bb8902919
Ludmilla <mariana.ludmilla@academico.ifpb.edu.br>
2025-05-23 20:24:58 -0300

    bandeirinhas

--- atividades-ls-2025/operacoes/operacao.print.js
+++ atividades-ls-2025/operacoes/operacao.print.js
@@ -1,7 +1,8 @@
-import { sum, product, sumOdds } from "./operacao.js";
+import { sum, soma, product, sumOdds } from "./operacao.js";
 
 let vetor = [1,2,3,4,5,6];
 
 console.log(sum(vetor));
 console.log(product(vetor));
 console.log(sumOdds(vetor));
+console.log(soma(vetor));
